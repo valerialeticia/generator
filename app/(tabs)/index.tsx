97e3bd231906096fa1ffd01be0960f957fe65d2f
valeria.luna <valeria.luna-ext@anbima.com.br>
2024-05-24 15:45:50 -0300

    feat: deixa dinamico a mudança de caracteres

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,42 @@
-import { Image, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
-import Slider from '@react-native-community/slider'
+import { Image, StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import Slider from "@react-native-community/slider";
+import { useState } from "react";
+
+let charset = "abcdefghijklmnopqrstuvxzABCDEFGHIJKLMNOPQRSTUVXZ0123456789";
 
 export default function HomeScreen() {
+  const [size, setSize] = useState(10);
+  const [passwordValue, setPasswordValue] = useState("");
+
+  const generatePassword = () => {
+    let password = "";
+
+    for (let i = 0, n = charset.length; i < size; i++) {
+      password += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    setPasswordValue(password);
+  };
+
   return (
     <View style={styles.container}>
-      <Image 
-        style={styles.logo}
-        source={require('@/assets/images/logo.png')}
-      />
-      <Text style={styles.title}>16 caracteres</Text>
+      <Image style={styles.logo} source={require("@/assets/images/logo.png")} />
+      <Text style={styles.title}>{size} caracteres</Text>
 
       <View style={styles.area}>
-        <Slider 
+        <Slider
           style={{ height: 50 }}
           minimumValue={6}
           maximumValue={20}
-          maximumTrackTintColor='#ff0000'
-          minimumTrackTintColor='#000'
-          thumbTintColor='#392de9'
+          maximumTrackTintColor="#ff0000"
+          minimumTrackTintColor="#000"
+          thumbTintColor="#392de9"
+          value={size}
+          onValueChange={(value) => setSize(Number(value.toFixed(0)))}
         />
       </View>
 
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={generatePassword}>
         <Text style={styles.buttonText}>Gerar senha</Text>
       </TouchableOpacity>
     </View>
@@ -31,9 +46,9 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f3f3ff'
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f3f3ff",
   },
   logo: {
     marginBottom: 60,
@@ -41,28 +56,28 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 30,
     lineHeight: 30,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   area: {
     marginTop: 14,
     marginBottom: 14,
-    width: '80%',
-    backgroundColor: '#fff',
+    width: "80%",
+    backgroundColor: "#fff",
     borderRadius: 8,
-    padding: 8
+    padding: 8,
   },
   button: {
-    backgroundColor: '#392de9',
-    width: '80%',
+    backgroundColor: "#392de9",
+    width: "80%",
     height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     borderRadius: 8,
-    marginBottom: 18
+    marginBottom: 18,
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 20,
-    fontWeight: 'bold'
-  }
+    fontWeight: "bold",
+  },
 });
