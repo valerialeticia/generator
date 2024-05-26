535eaf02bb63922046675b5e809965b1b1c93f32
valerialeticia <valerialeticialuna1@gmail.com>
2024-05-26 17:52:01 -0300

    feat: otimiza generatePassword

--- hooks/useHome/useHome.ts
+++ hooks/useHome/useHome.ts
@@ -1,21 +1,22 @@
-import { useState } from "react";
-
-let charset = "abcdefghijklmnopqrstuvxzABCDEFGHIJKLMNOPQRSTUVXZ0123456789";
+import { useState } from 'react'
 
 const useHome = () => {
-  const [size, setSize] = useState(10);
-  const [passwordValue, setPasswordValue] = useState("");
-  const [modalVisible, setModalVisible] = useState(false);
+  const [size, setSize] = useState(10)
+  const [passwordValue, setPasswordValue] = useState('')
+  const [modalVisible, setModalVisible] = useState(false)
 
   const generatePassword = () => {
-    let password = "";
+    let charset = 'abcdefghijklmnopqrstuvxzABCDEFGHIJKLMNOPQRSTUVXZ0123456789'
 
-    for (let i = 0, n = charset.length; i < size; i++) {
-      password += charset.charAt(Math.floor(Math.random() * n));
-    }
+    const passwordArray = Array.from({ length: size }, () => {
+      const randomIndex = Math.floor(Math.random() * charset.length)
+      return charset.charAt(randomIndex);
+    })
 
-    setPasswordValue(password);
-    setModalVisible(true);
+    const password = passwordArray.join('')
+
+    setPasswordValue(password)
+    setModalVisible(true)
   };
 
   return {
